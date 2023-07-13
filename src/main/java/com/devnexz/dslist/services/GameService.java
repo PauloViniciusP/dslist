166d0ec079ed591f10e030d4878a3486cb4436cd
Paulo Nascimento <nexz265@gmail.com>
2023-07-12 22:01:49 -0300

    GET game by id

--- src/main/java/com/devnexz/dslist/services/GameService.java
+++ src/main/java/com/devnexz/dslist/services/GameService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devnexz.dslist.dto.GameDTO;
 import com.devnexz.dslist.dto.GameMinDTO;
 import com.devnexz.dslist.entities.Game;
 import com.devnexz.dslist.repositories.GameRepository;
@@ -13,6 +16,14 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findALL() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
